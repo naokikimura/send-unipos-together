b9a802ab2c95e09977dd66d19261d68b32acb0ea
Naoki Kimura <n.kimura.cap@gmail.com>
2020-01-14 21:19:39 +0900

    Promisification

--- popup.js
+++ popup.js
@@ -1,15 +1,74 @@
 import UniposAPI from './unipos/api.js';
 
-const executeScript = (...args) => new Promise((resolve, reject) => {
-  chrome.tabs.executeScript(...args.concat(resolve));
-});
+const newRejectedPromise = (errorConstructor, message) => Promise.reject(new errorConstructor(message));
+
+function promisify(original, makeCallback) {
+  function defaultMakeCallback(resolve, reject) {
+    return function callback(error, result) {
+      if (error)
+        reject(error);
+      else
+        resolve(result);
+    }
+  }
+  return function (...args) {
+    const that = this;
+    return new Promise(function executor(resolve, reject) {
+      original.apply(that, args.concat((makeCallback || defaultMakeCallback)(resolve, reject)));
+    });
+  };
+}
+
+const makeCallback = function (resolve, reject) {
+  return function callback(...args) {
+    const [results] = args;
+    const error = chrome.runtime.lastError;
+    if (error)
+      reject(error);
+    else
+      resolve(results);
+  };
+};
+
+const storageArea = ((global, storageType) => {
+  if (global.hasOwnProperty('browser')) {
+    return global.browser.storage[storageType];
+  } else if (global.hasOwnProperty('chrome')) {
+    const storageArea = global.chrome.storage[storageType];
+    return new class StorageArea {
+      get(keys) { return promisify(storageArea.get.bind(storageArea), makeCallback)(keys); }
+      getBytesInUse(keys) { return promisify(storageArea.getBytesInUse.bind(storageArea), makeCallback)(keys); }
+      set(keys) { return promisify(storageArea.set.bind(storageArea), makeCallback)(keys); }
+      remove(keys) { return promisify(storageArea.remove.bind(storageArea))(keys); }
+      clear() { return promisify(storageArea.clear.bind(storageArea), makeCallback)(); }
+    }();
+  } else {
+    const reject = () => newRejectedPromise(ReferenceError, 'browser or chrome is not defined');
+    return new class {
+      get(keys) { return reject(); }
+      getBytesInUse(keys) { return reject(); }
+      set(keys) { return reject(); }
+      remove(keys) { return reject(); }
+      clear() { return reject(); }
+    }();
+  }
+})(window, 'sync');
+
+function executeScriptAsync(...args) {
+  if (window.hasOwnProperty('browser'))
+    return browser.tabs.executeScript(...args);
+  else if (window.hasOwnProperty('chrome'))
+    return promisify(chrome.tabs.executeScript.bind(chrome.tabs), makeCallback)(...args);
+  else
+    return newRejectedPromise(ReferenceError, 'browser or chrome is not defined');
+};
 
 const api = new UniposAPI({
-  load: () => executeScript({
+  load: () => executeScriptAsync({
     code: "[window.localStorage.getItem('authnToken'), window.localStorage.getItem('refreshToken')]"
   }).then(results => results[0]),
 
-  save: (authnToken, refreshToken) => executeScript({
+  save: (authnToken, refreshToken) => executeScriptAsync({
     code: `window.localStorage.setItem('authnToken', '${authnToken}'); window.localStorage.setItem('refreshToken', '${refreshToken}');`
   })
 });
@@ -87,10 +146,10 @@ window.addEventListener('DOMContentLoaded', (event) => {
     });
   }
 
-  chrome.storage.sync.get(['options'], result => {
+  storageArea.get(['options']).then(result => {
     const { recipientMembers = [], point = null, message = '' } = result.options || {};
     recipients.appendMember(...recipientMembers);
     document.querySelector('fieldset#card [name="point"]').value = point === null ? '' : point;
     document.querySelector('fieldset#card [name="message"]').value = message;
-  });
+  }).catch(console.error);
 });
