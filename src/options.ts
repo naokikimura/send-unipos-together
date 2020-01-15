660f85d26b8d2d4ba3ba4142d555458783032d54
Naoki Kimura <n.kimura.cap@gmail.com>
2020-01-16 03:05:11 +0900

    Fix warning from TSLint.

--- src/options.ts
+++ src/options.ts
@@ -1,18 +1,18 @@
-window.addEventListener('DOMContentLoaded', (event) => {
+window.addEventListener('DOMContentLoaded', () => {
   chrome.storage.sync.get(['options'], result => {
     const { point = null, message = '' } = result.options || {};
     document.querySelector<HTMLInputElement>('form#options [name="point"]').value = point === null ? '' : point;
     document.querySelector<HTMLTextAreaElement>('form#options [name="message"]').value = message;
   });
 
-  document.querySelector<HTMLFormElement>('form#options').addEventListener('change', function (event) {
+  document.querySelector<HTMLFormElement>('form#options').addEventListener('change', event => {
     const form = event.currentTarget as HTMLFormElement;
     if (!form.reportValidity()) return;
     const data = new FormData(form);
     const options = {
+      message: data.get('message'),
       point: data.get('point'),
-      message: data.get('message')
     };
-    chrome.storage.sync.set({ options: options });
+    chrome.storage.sync.set({ options });
   });
 });
