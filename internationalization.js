ec638f73d2ccbfcb8d7e3ab2c55e6119e3c95c2c
Naoki Kimura <n.kimura.cap@gmail.com>
2020-01-15 15:50:53 +0900

    Fix a bug

--- internationalization.js
+++ internationalization.js
@@ -1,30 +1,33 @@
+// See https://tc39.es/ecma402/#sec-bestavailablelocale
+function bestAvailableLocale(availableLocales, locale) {
+  let candidate = locale;
+  while (true) {
+    if (availableLocales.includes(candidate)) return candidate;
+    const pos = candidate.lastIndexOf('-');
+    if (pos === -1) return undefined;
+    candidate = candidate.substring(0, pos);
+  }
+};
+
+// https://tc39.es/ecma402/#sec-lookupmatcher
+function lookupMatcher(availableLocales, requestedLocales) {
+  for (const locale of requestedLocales) {
+    const noExtensionsLocale = String(locale).replace(/-u(?:-[0-9a-z]{2,8})+/gi, '');
+    const availableLocale = bestAvailableLocale(availableLocales, noExtensionsLocale);
+    if (availableLocale !== undefined) return availableLocale;
+  }
+  return this.defaultLocale;
+};
+
 export default class Internationalization {
   constructor(messages, defaultLocale, ...requestedLocales) {
-    // See https://tc39.es/ecma402/#sec-bestavailablelocale
-    const bestAvailableLocale = (availableLocales, locale) => {
-      let candidate = locale;
-      while(true) {
-        if (availableLocales.includes(candidate)) return candidate;
-        const pos = candidate.lastIndexOf('-');
-        if (pos === -1) return undefined;
-        candidate = candidate.substring(0, pos);
-      }
-    };
-
-    // https://tc39.es/ecma402/#sec-lookupmatcher
-    const lookupMatcher = (availableLocales, requestedLocales) => {
-      for (const locale of requestedLocales) {
-        const noExtensionsLocale = (new window.Intl.Locale(locale)).baseName;
-        const availableLocale = bestAvailableLocale(availableLocales, noExtensionsLocale);
-        if (availableLocale !== undefined) return availableLocale;
-      }
-      return defaultLocale;
-    };
-    const locale = lookupMatcher(Object.keys(messages), requestedLocales);
-    this._messages = messages[locale.toString()];
+    const locale = lookupMatcher.call({ defaultLocale }, Object.keys(messages), requestedLocales);
+    this._messages = messages[locale.toString()] || [];
+    this._defaulMessages = messages[defaultLocale] || [];
   }
 
   getMessage(messageName) {
-    return this._messages[messageName] && this._messages[messageName].message;
+    const message = this._messages[messageName] && this._messages[messageName].message;
+    return message || this._defaulMessages[messageName] && this._defaulMessages[messageName].message;
   }
 }
