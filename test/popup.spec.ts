4b77c04a8551c0aaf50a19ce60efc519cfe9112a
Naoki KIMURA <n.kimura.cap@gmail.com>
2025-01-14 12:12:33 +0900

    Update package.json

--- test/popup.spec.ts
+++ test/popup.spec.ts
@@ -1,9 +1,13 @@
 import { expect } from 'chai';
 import * as puppeteer from 'puppeteer';
+import { debuglog } from 'node:util';
+
+const debug = debuglog('send-unipos-together');
+
+const EXTENTION_ID = 'pgpnkghddnfoopjapnlklllpjknnibkn';
 
 (process.env.TRAVIS ? describe.skip : describe)('popup', () => {
   before(async function before() {
-    this.timeout(5000);
     const extensionPath = process.cwd();
     const browser = await puppeteer.launch({
       args: [
@@ -16,12 +20,7 @@ import * as puppeteer from 'puppeteer';
 
     const extensionPage = await browser.newPage();
     const extensionPopupHtml = 'resources/popup.html';
-    const extensionID = ((targets): string => {
-      const extensionTarget = targets.find(target => target.type() === 'background_page');
-      const [, , id] = extensionTarget.url().split('/');
-      return id;
-    })(await browser.targets());
-    await extensionPage.goto(`chrome-extension://${extensionID}/${extensionPopupHtml}`);
+    await extensionPage.goto(`chrome-extension://${EXTENTION_ID}/${extensionPopupHtml}`);
     this.extensionPage = extensionPage;
   });
 
@@ -33,8 +32,8 @@ import * as puppeteer from 'puppeteer';
   it('it should return a page title', async function it() {
     const extensionPage = (this.extensionPage as puppeteer.Page);
     const title = await extensionPage.$('title')
-      .then(element => element.getProperty('textContent'))
-      .then(property => property.jsonValue());
+      .then(element => element?.getProperty('textContent'))
+      .then(property => property?.jsonValue());
     expect(title).to.equal('Send Unipos together');
   });
 });
